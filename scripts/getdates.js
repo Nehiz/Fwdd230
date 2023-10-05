c271619fdca468c4894f7db8726013bd154b59bc
Nehiz <nehis4all@yahoo.com>
2023-10-05 17:47:19 +0100

    Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,16 @@
 const currentYear = new Date().getFullYear();
 
 // Get the element with the id "copyright-year"
-const yearElement = document.querySelector("#copyright-year");
+const yearElement = document.querySelector("#datetime");
 
 // Insert the current year into the yearElement
 yearElement.innerHTML = currentYear;
+
+// Get the date and time the document was last modified
+const lastModified = document.lastModified;
+
+// Get the element with the id "lastModified"
+const modifiedElement = document.querySelector("#lastModified");
+
+// Insert the last modified date into the modifiedElement
+modifiedElement.innerHTML = lastModified;
